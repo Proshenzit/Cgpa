55b200307b8958c340659096678b4728ddfa6da2
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-15 12:16:03 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,24 @@
+var Marks = prompt("Enter  your Marks");
+
+if (Marks >= 80)
+
+    console.log("A+");
+
+else if (Marks >= 70)
+
+    console.log("A");
+else if (Marks >= 60)
+
+    console.log("A-");
+else if (Marks >= 50)
+
+    console.log("B");
+else if (Marks >= 40)
+
+    console.log("C");
+else if (Marks >= 33)
+
+    console.log("D");
+
+else
+    console.log("Fail");
